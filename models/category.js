37893f4a1d836f26ba17cdb01f58a03d5e07cb45
theo82 <theoaristi53@gmail.com>
2021-05-05 16:57:50 +0300

    made some modifications

--- models/category.js
+++ models/category.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-    name: {
-        type: String, 
-        required: true
-    },
-    icon: {
-        type: String
-    },
-    color: {
-        type: String
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
+categorySchema.method('toJSON', function () {
+  const { __v, ...object } = this.toObject();
+  const { _id: id, ...result } = object;
+  return { ...result, id };
+});
 
 exports.Category = mongoose.model('Category', categorySchema);
